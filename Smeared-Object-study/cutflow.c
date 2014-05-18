3db8ea58d88925b8d5331e86d30ec8d8d7ba5557
ytchoutw <yuantangchou@hotmail.com>
2014-05-19 04:27:13 +0800

    Update cutflow.c

--- Smeared-Object-study/cutflow.c
+++ Smeared-Object-study/cutflow.c
@@ -26,7 +26,8 @@ void cutflow(char* inputFile){
     TChain* tc = new TChain("Delphes");
     tc->Add(inputFile);
     ExRootTreeReader* tr = new ExRootTreeReader(tc);
-    TH1F *h1 = new TH1F("Cut flow","Cut flow",101,0,100);
+    TH1F *h1 = new TH1F("Cut flow ,r","Cut flow ,r",101,0,100);
+    TH1F *h2 = new TH1F("Cut flow ,b","Cut flow ,b",111,0,110);
 
     //Create object of class ExRootTreeReader
     Long64_t numberofEntries = tr->GetEntries();
@@ -34,23 +35,28 @@ void cutflow(char* inputFile){
     //Get pointer to branches
     TClonesArray* branchElectron = tr->UseBranch("Electron");
     TClonesArray* branchJet4 = tr->UseBranch("Jet4");
-    TClonesArray* branchJet6 = tr->UseBranch("Jet6");
-    TClonesArray* branchJet10 = tr->UseBranch("Jet10");
+    TClonesArray* branchJet8 = tr->UseBranch("Jet8");
+    TClonesArray* branchJet12 = tr->UseBranch("Jet12");
     TClonesArray* branchMuon = tr->UseBranch("Muon");
     //TClonesArray* branchParticle = tr->UseBranch("Particle");
 
 
-    double electronMinPt = 25.0;
-    double electronMaxEta = 2.4;
-    double muonMinPt = 25.0;
-    double muonMaxEta = 2.4;
+    double electronMinPt = 5.0;
+    double electronMaxEta = 2.5;
+    double muonMinPt = 5.0;
+    double muonMaxEta = 2.5;
     double jetMinPt = 25.0;
+    double jet8MinPt = 315.0;
+    double jet12MinPt = 210.0;
 
-    double jetMaxEta = 2;
+
+    double jetMaxEta = 2.0;
     double dileptonMaxmass = 100;
     double dileptonMinmass = 80;
     double dijetMaxmass = 141;
     double dijetMinmass = 111;
+    double boostjetMaxmass = 146;
+    double boostjetMinmass = 106;
 
 
 
@@ -63,16 +69,16 @@ void cutflow(char* inputFile){
 
     //all the events
     h1->Fill(10);
+    h2->Fill(10);
     bool positive = false;
     bool negative = false;
-    int leadingleptonAt = 0;
-    int subleadingleptonAt = 0;
+    
 
     std::vector<Electron> goodElectrons;
     std::vector<Muon> goodMuons;
-    std::vector<Jet> goodJets6;
+    std::vector<Jet> goodJets8;
     std::vector<Jet> goodJets4;
-    std::vector<Jet> goodJets10;
+    std::vector<Jet> goodJets12;
 
     //events have a pair of lepton pt > minPT , |Eta| < MaxEta
     for(int i=0; i < branchElectron->GetEntries() ;++i){
@@ -103,11 +109,11 @@ void cutflow(char* inputFile){
     }
     int leptonNumber = 0;
     double dileptonMass = 0;
-    std::cout << goodElectrons.size() << "  "  <<  goodMuons.size() << std::endl;
+    //std::cout << goodElectrons.size() << "  "  <<  goodMuons.size() ;
     if (goodElectrons.size() == 2 && goodMuons.size() == 0) {
-    	dileptonMass = (goodElectrons[0].P4() + goodElectrons[1].P4()).M();    
+    	dileptonMass = (goodElectrons[0].P4() + goodElectrons[1].P4()).M();
 	leptonNumber = 2 ;
-	
+
     }
     else if (goodElectrons.size() == 0 && goodMuons.size() == 2) {
         leptonNumber = 2 ;
@@ -116,82 +122,152 @@ void cutflow(char* inputFile){
 
     if( positive == false || negative == false|| leptonNumber != 2){    continue;    }
     h1->Fill(20);
+    h2->Fill(20);
 
 
     // minMass< m(ll) < maxMass
     if(dileptonMass < dileptonMinmass || dileptonMass > dileptonMaxmass ){  continue;  }
     h1->Fill(30);
+    h2->Fill(30);
 
-
-    // at least two jet4 (jet pT>25GeV, |eta|<2)
-    double numerofgoodJet4 = 0;
-    for(int i=0;i<branchJet4->GetEntries();++i){
-            Jet* jet4=(Jet*) branchJet4->At(i);
-            if(jet4->PT > jetMinPt && fabs(jet4->Eta) < jetMaxEta){
-                numerofgoodJet4++;
-            }
-    }
-
-    if(numerofgoodJet4<2){continue; }
-    h1->Fill(40);
-
-
-
-    // at least two b-jet4 (jet pT>25GeV, |eta|<2)
     float electronJetCloseness = 0.1;
     for (int i = 0; i < branchJet4->GetEntries(); ++i) {
-      Jet* jet =(Jet *) branchJet4->At(i);
-      if (jet->PT > jetMinPt && fabs(jet->Eta) < jetMaxEta) {
-      // electrons are detected as jets too
-      // we want to skip these, so we will compare each jet to the previously found electrons
-      // and skip it if it is one of them
-          bool electronJetMatch = false;
-          for (size_t ii = 0; ii < goodElectrons.size(); ++ii) {
-             if (goodElectrons[ii].P4().DeltaR(jet->P4()) < electronJetCloseness){
-                 electronJetMatch = true;
-             }
-          }
-          if (!electronJetMatch) {
-             goodJets4.push_back(*jet);
-          }
-      }
-    }
-    
-
-    //select goodJet6 (jet pT>25GeV, |eta|<2)
-    for (int i = 0; i < branchJet6->GetEntries(); ++i) {
-      Jet* jet = (Jet *) branchJet6->At(i);
-      if (jet->PT > jetMinPt && fabs(jet->Eta) < jetMaxEta) {
-        bool electronJetMatch = false;
-          for (size_t ii = 0; ii < goodElectrons.size(); ++ii) {
-             if (goodElectrons[ii].P4().DeltaR(jet->P4()) < electronJetCloseness){
-                 electronJetMatch = true;
-             }
-          }
-          if (!electronJetMatch) {
-             goodJets6.push_back(*jet);
-          }
-      }
+        Jet* jet =(Jet *) branchJet4->At(i);
+        if (jet->PT > jetMinPt && fabs(jet->Eta) < jetMaxEta) {
+            // electrons are detected as jets too
+            // we want to skip these, so we will compare each jet to the previously found electrons
+            // and skip it if it is one of them
+            bool electronJetMatch = false;
+            for (size_t ii = 0; ii < goodElectrons.size(); ++ii) {
+                if (goodElectrons[ii].P4().DeltaR(jet->P4()) < electronJetCloseness){
+                    electronJetMatch = true;
+                }
+            }
+            if (!electronJetMatch) {
+                goodJets4.push_back(*jet);
+            }
+        }
     }
 
-    if(goodJets4.size() < 2){  continue;    }
-    h1->Fill(50);
+    bool resolved = false ;
+    // at least two jet4 (jet pT>25GeV, |eta|<2)
+    //resolved channel
+    if(goodJets4.size() >= 2 ){
+        h1->Fill(40);
+        std::vector<Jet> btagJet4;
+        for(size_t i=0 ; i < goodJets4.size() ; i++){
+            if(goodJets4[i].BTag == 1){
+                btagJet4.push_back(goodJets4[i]);
+            }
+        }
+	//std::cout << " batg4 " << btagJet4.size() ;
 
-    // 111GeV < m(jj) <141 GeV
-    double dijet4mass = (goodJets4[0].P4()+goodJets4[1].P4()).M();
-    double dijet6mass = (goodJets6[0].P4()+goodJets6[1].P4()).M();
-    if((dijet4mass > dijetMaxmass || dijet4mass < dijetMinmass)){   continue;    }
-    h1->Fill(60);
+        // at least two b-jet4 (jet pT>25GeV, |eta|<2)
+        if( btagJet4.size() >=2 ){
+            h1->Fill(50);
+            // 111GeV < m(jj) <141 GeV
+            double dijet4mass = (btagJet4[0].P4()+btagJet4[1].P4()).M();
+            if((dijet4mass > dijetMaxmass || dijet4mass < dijetMinmass)){
+            h1->Fill(60);
+	    resolved = true;
+            }
+	    
+        }
 
-  
+    }
+
+    //merge channel
+    if(resolved == false ){
+        h2->Fill(40);
+
+
+        //select goodJet8 (jet pT>315GeV, |eta|<2)
+        for (int i = 0; i < branchJet8->GetEntries(); ++i) {
+            Jet* jet = (Jet *) branchJet8->At(i);
+            if (jet->PT > jet8MinPt && fabs(jet->Eta) < jetMaxEta) {
+                bool electronJetMatch = false;
+                for (size_t ii = 0; ii < goodElectrons.size(); ++ii) {
+                    if (goodElectrons[ii].P4().DeltaR(jet->P4()) < electronJetCloseness){
+                    electronJetMatch = true;
+                    }
+                }
+                if (!electronJetMatch) {
+                    goodJets8.push_back(*jet);
+                }
+            }
+        }
+        //select goodJet12 (jet pT>210GeV, |eta|<2)
+        for (int i = 0; i < branchJet12->GetEntries(); ++i) {
+            Jet* jet = (Jet *) branchJet12->At(i);
+            if (jet->PT > jet12MinPt && fabs(jet->Eta) < jetMaxEta) {
+                bool electronJetMatch = false;
+                for (size_t ii = 0; ii < goodElectrons.size(); ++ii) {
+                    if (goodElectrons[ii].P4().DeltaR(jet->P4()) < electronJetCloseness){
+                    electronJetMatch = true;
+                    }
+                }
+                if (!electronJetMatch) {
+                    goodJets12.push_back(*jet);
+                }
+            }
+        }
+
+        //at least 1 Jet8
+        if(goodJets8.size() >= 1){
+            h2->Fill(50);
+            std::vector<Jet> btagJet8;
+            for(size_t i=0 ; i < goodJets8.size() ; i++){
+                if(goodJets8[i].BTag == 1){
+                    btagJet8.push_back(goodJets8[i]);
+                }
+            }
+            //std::cout << "  batg8 " << btagJet8.size() ;
+            //at least 1 b-tagged jet8
+            if(btagJet8.size() >= 1 ){
+                h2->Fill(60);
+                //if(btagJet8.size() > 1){std::cout << "error8" ;}
+
+                 //boost jet8 mass  in 106GeV 146GeV
+                double jet8Mass = btagJet8[0].P4().M();
+                if( jet8Mass < boostjetMaxmass && jet8Mass > boostjetMinmass ){
+                    h2->Fill(70);
+                }
+
+            }
+
+        }
+
+        //at lest 1 jet 12
+        if(goodJets12.size() >= 1){
+            h2->Fill(80);
+            std::vector<Jet> btagJet12;
+            for(size_t i=0 ; i < goodJets12.size() ; i++){
+                if(goodJets12[i].BTag == 1){
+                    btagJet12.push_back(goodJets12[i]);
+                }
+            }
+            //std::cout << "  batg12 " << btagJet12.size();
+
+            //at least 1 b-tagged jet12
+            if(btagJet12.size() >= 1){
+                h2->Fill(90);
+		//if(btagJet12.size() > 1){std::cout<< "error12" ;}
+                //boost jet12 mass in 106GeV 146GeV
+                double jet12Mass = btagJet12[0].P4().M();
+                if( jet12Mass < boostjetMaxmass && jet12Mass > boostjetMinmass ){
+                    h2->Fill(100);
+                }
+
+            }
+
+        }
+
+
+    }
+	
+  //std::cout << std::endl ;
 
   }
-  
-  h1->Draw();
-  //TAxis *x = h->GetXaxis()();
-  //float t = h1->GetBinContent(10.1);
-  //std::cout << t << std::endl ;
-  //std::cout << h1->GetBinContent(10) << std::end;
 
+  h2->Draw();
 }
-
